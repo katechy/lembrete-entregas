bf783e7b1139a22659f762962c821327238a0d8a
katechy <116845151+katechy@users.noreply.github.com>
2026-05-24 16:53:03 -0300

    Update check_entregas.py

--- check_entregas.py
+++ check_entregas.py
@@ -10,7 +10,6 @@ WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK_URL')
 ENTREGAS = [
     {"disciplina": "Tópicos Especiais em Design de Animação I", "atividade": "Atividades e prova", "data_entrega": "2026-06-01"},
     {"disciplina": "Temas Transversais", "atividade": "Atividades e prova", "data_entrega": "2026-06-16"},
-    {"disciplina": "Projeto Integrado em Design de Animação Game", "atividade": "Projeto", "data_entrega": "2026-05-25"},
     {"disciplina": "Linguagem Fotográfica", "atividade": "Atividades e Prova", "data_entrega": "2026-06-01"},
     {"disciplina": "Design Sustentável e Responsabilidade Social", "atividade": "Atividades e Prova", "data_entrega": "2026-06-01"},
     {"disciplina": "Estética e Linguagem Audiovisual", "atividade": "Atividades e Prova", "data_entrega": "2026-06-01"},
